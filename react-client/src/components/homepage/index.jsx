9879185837469451f7ea0b17a0d0337b81df960a
Emily Yu <emilyyu518@gmail.com>
2018-02-08 17:01:22 -0600

    (scaffolding) Sets up homepage index

---
+++ react-client/src/components/homepage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Home extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <header>
+        Dynanner
+      </header>
+    );
+  }
+}
+
+export default Home;
