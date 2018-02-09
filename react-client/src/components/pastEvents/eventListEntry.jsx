e618bff0ed44161abe7a5f9ef2c6bbb4dc76fdcf
Emily Yu <emilyyu518@gmail.com>
2018-02-08 19:43:00 -0600

    (scaffolding) builds out basic scaffolding for pastEvents views

---
+++ react-client/src/components/pastEvents/eventListEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventListEntry = () => {
+  return (
+    <div>
+      {/* date */}
+      {/* event list entry name */}
+    </div>
+  );
+}
+
+export default EventListEntry;
---
+++ react-client/src/components/pastEvents/eventsList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EventListEntry from './eventListEntry';
+
+class EventsList extends React.component {
+  render() {
+    return (
+      <div>
+        <EventListEntry />
+      </div>
+    );
+  }
+}
+
+export default EventsList;
