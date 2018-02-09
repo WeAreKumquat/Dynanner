cdc7f1cf135c506cb64f67b6dcb9ad0113460e71
Emily Yu <emilyyu518@gmail.com>
2018-02-08 18:34:49 -0600

    (scaffolding) basic scaffolding for addEvent view

---
+++ react-client/src/components/addEvent/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AddEvent extends React.component {
+  constructor(props) {
+    super(props) {
+      this.state = {
+
+      };
+    };
+  }
+  render() {
+    return (
+      <div>
+        {/* add event form inputs */}
+        {/* Google Calendar */}
+      </div>
+    )
+  }
+};
+
+export default AddEvent;
