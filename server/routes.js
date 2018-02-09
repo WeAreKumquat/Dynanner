f515cc6011a62eeec8e80ee0bfc9c44e7373af08
jsmichot <jsmichot@gmail.com>
2018-02-08 20:39:35 -0600

    (server) adds routes scaffolding

---
+++ server/routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const request = require('request');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-auth').Strategy;
+const controller = require('./controllers');
+
+router.get('/', (req, res) => {});
+router.get('/login', (req, res) => {});
+router.get('/homepage', (req, res) => {});
+router.get('/addEvent', (req, res) => {});
+router.get('/pastEvents', (req, res) => {});
+router.get('/reviewEvent', (req, res) => {});
+
+router.post('/addEvent', (req, res) => {});
+router.post('/reviewEvent');
+router.post('/newFeedback');
+
+module.exports = router;
