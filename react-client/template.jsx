bf5fbcbb338118bfd196a50c336f8773df21ec80
Emily Yu <emilyyu518@gmail.com>
2018-02-14 16:10:51 -0600

    (upcoming-events) added and linked css stylesheet

--- react-client/template.jsx
+++ react-client/template.jsx
@@ -14,12 +14,13 @@ export default ({ body, title, initialState }) =>
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
         crossorigin="anonymous">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oleo+Script+Swash+Caps">
-      <link rel="stylesheet" href="./dist/styles.css">
+      <link rel="stylesheet" href="styles.css">
     </head>
     <body>
       <div id="root">
         ${body}
       </div>
+      <script src="bundle.js" />
     </body>
   </html>`;
 
