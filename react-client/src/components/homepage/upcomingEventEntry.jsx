19062f1092e4bee466393233b7aa4f774f84bc94
Emily Yu <emilyyu518@gmail.com>
2018-02-15 19:07:44 -0600

    (homepage) upcoming event entries have collapsible descriptions

--- react-client/src/components/homepage/upcomingEventEntry.jsx
+++ react-client/src/components/homepage/upcomingEventEntry.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 const UpcomingEventEntry = ({ event, key }) => {
   const { title, description } = event;
-  console.log('title', title);
-  console.log('desc', description);
+  const id = `#${title}`;
+
   return (
-    <div>
-      Title: {title}
-      <br />
-      Description: {description}
-      {/* upcoming event */}
+    <div className="card">
+      <div className="card-header" id="headingOne">
+        <h5 className="mb-0">
+          <button className="btn btn-link collapsed" data-toggle="collapse" data-target={id} aria-expanded="false" aria-controls="collapseOne">
+            {title}
+          </button>
+        </h5>
+      </div>
+
+      <div id={title} className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+        <div className="card-body">
+          {description}
+        </div>
+      </div>
     </div>
   );
 };
