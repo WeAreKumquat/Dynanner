d408d479149f46831c98762f856d759ac72f14e4
Emily Yu <emilyyu518@gmail.com>
2018-02-16 15:07:27 -0600

    (calendar) adds user's Google calendar to add event page

--- react-client/src/components/addEvent/index.jsx
+++ react-client/src/components/addEvent/index.jsx
@@ -12,7 +12,7 @@ class AddEvent extends React.Component {
       title: 'event',
       date: '2018-04-15',
       description: 'just do it',
-      email: 'address@domainName.com',
+      calSrc: '',
       redirect: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -28,9 +28,8 @@ class AddEvent extends React.Component {
     axios.get('/api/getEmail')
       .then((email) => {
         const emailUserCal = email.data.replace('@', '%40');
-        this.setState({
-          email: emailUserCal
-        });
+        const calSrc = `https://calendar.google.com/calendar/embed?src=${emailUserCal}&ctz=America%2FChicago`;
+        this.setState({ calSrc });
       })
       .catch((error) => { console.log(`Error trying to get user's email: ${error}`); });
   }
@@ -69,52 +68,47 @@ class AddEvent extends React.Component {
     });
   }
   render() {
-    const { redirect } = this.state;
+    const { redirect, calSrc } = this.state;
     return (
       <div className="body">
         <h1>Add an Event</h1>
-        <div>
-          <select name="category" onChange={this.handleChange} value={this.state.category}>
-            <option value="work">work</option>
-            <option value="play">play</option>
-          </select>
-          <br />
-          <div>
-            Title: &ensp;
-            <input type="text" onChange={this.handleChange} name="title" ref="title" />
+        <div className="container-fluid">
+          <div className="row justify-content-between">
+            <div className="col-md-6 col-lg-4">
+              <select name="category" onChange={this.handleChange} value={this.state.category}>
+                <option value="work">work</option>
+                <option value="play">play</option>
+              </select>
+              <br />
+              <div>
+                Title: &ensp;
+                <input type="text" onChange={this.handleChange} name="title" ref="title" />
+              </div>
+              <br />
+              <div>
+                Date: &ensp;
+                <DatePicker id="example-datepicker" dateFormat="YYYY-MM-DD" value={this.state.date} onChange={this.changeDate}
+                  minDate="2018-02-16T12:00:00.000Z"
+                  maxDate="2118-01-01T12:00:00.000Z"
+                />
+              </div>
+              <br />
+              <div>
+                Description: &ensp;
+                <input type="text" onChange={this.handleChange} name="description" ref="description" />
+              </div>
+              <br />
+              <button type="submit" onClick={this.handleSubmit}>
+                Submit
+              </button>
+            </div>
+            <div className="col-md-6 col-lg-8">
+              <iframe title="user-calendar" src={calSrc} width="800" height="600" frameBorder="0" scrolling="no" />
+            </div>
           </div>
-          <br />
-          <div>
-            Date: &ensp;
-            <DatePicker id="example-datepicker" dateFormat="YYYY-MM-DD" value={this.state.date} onChange={this.changeDate}
-              minDate="2018-02-16T12:00:00.000Z"
-              maxDate="2118-01-01T12:00:00.000Z"
-            />
-          </div>
-          <br />
-          <div>
-            Description: &ensp;
-            <input type="text" onChange={this.handleChange} name="description" ref="description" />
-          </div>
-          <br />
-          <button type="submit" onClick={this.handleSubmit}>
-            Submit
-          </button>
         </div>
-        <iframe
-          title="userCal"
-          src="https://www.google.com/calendar/embed?height=600&amp;
-          wkst=1&amp;bgcolor=%23FFFFFF&amp;
-          src={this.state.email}&amp;
-          color=%232952A3&amp;
-          ctz=Europe%2FParis"
-          width="800"
-          height="600"
-          frameBorder="0"
-          scrolling="no"
-        />
         {redirect && (
-          <Redirect to={'/pastEvents'} component={PastEvents} />
+          <Redirect to="/pastEvents" component={PastEvents} />
         )}
       </div>
     );
